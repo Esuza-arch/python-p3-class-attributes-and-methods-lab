0a94e253281ea2d471358181cf7ac56f8298f633
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-12-10 21:05:36 +0300

    Complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,34 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update class attributes
+        Song.count += 1
+
+        # Update genres and artists lists
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # Update genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Update artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
